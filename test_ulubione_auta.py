1467e7e9f392a15534a27b63b5efa37c4a10a41c
Krzysztofgw <Wies.niaK1>
2022-11-06 17:05:25 +0100

    Uzupelniono wszystkie testy.

--- test_ulubione_auta.py
+++ test_ulubione_auta.py
@@ -60,22 +60,131 @@ Marka: BMW, Nazwa: E30, Rok produkcji: 2005 - 1
             self.assertEqual(text.getvalue(), expected)
 
     def test_usun_duplikaty(self):
-        pass
+        lista = UlubioneAuta()
+
+        lista.dodaj_auto("Kia", "Ceed", 2001)
+        lista.dodaj_auto("Kia", "Ceed", 2001)
+        lista.dodaj_auto("Kia", "Ceed", 2001)
+        lista.dodaj_auto("BMW", "E30", 2005)
+        lista.dodaj_auto("BMW", "E30", 2005)
+        lista.dodaj_auto("Fiat", "500x", 2005)
+        lista.dodaj_auto("Fiat", "500x", 2005)
+        lista.dodaj_auto("Fiat", "Croma", 2020)
+        lista.dodaj_auto("Fiat", "Croma", 2020)
+        lista.dodaj_auto("Ford", "Fusion", 2005)
+        lista.dodaj_auto("Ford", "Fiesta", 2005)
+        lista.usun_duplikaty()
+        self.assertEqual(lista.auta, [Auto("Kia", "Ceed", 2001),
+                                      Auto("BMW", "E30", 2005),
+                                      Auto("Fiat", "500x", 2005),
+                                      Auto("Fiat", "Croma", 2020),
+                                      Auto("Ford", "Fusion", 2005),
+                                      Auto("Ford", "Fiesta", 2005)])
 
     def test_sortuj_nazwa(self):
-        pass
+        lista = UlubioneAuta()
+
+        lista.dodaj_auto("Fiat", "Croma", 2020)
+        lista.dodaj_auto("Ford", "Fusion", 2005)
+        lista.dodaj_auto("Ford", "Fiesta", 2005)
+        lista.dodaj_auto("BMW", "E30", 2005)
+        lista.dodaj_auto("Kia", "Ceed", 2001)
+
+        sorted_list = lista.sortuj_nazwa()
+        self.assertEqual(sorted_list, [
+            Auto("Kia", "Ceed", 2001),
+            Auto("Fiat", "Croma", 2020),
+            Auto("BMW", "E30", 2005),
+            Auto("Ford", "Fiesta", 2005),
+            Auto("Ford", "Fusion", 2005)
+        ])
 
     def test_sortuj_nazwa_odwrotnie(self):
-        pass
+        lista = UlubioneAuta()
+
+        lista.dodaj_auto("Fiat", "Croma", 2020)
+        lista.dodaj_auto("Ford", "Fusion", 2005)
+        lista.dodaj_auto("Ford", "Fiesta", 2005)
+        lista.dodaj_auto("BMW", "E30", 2005)
+        lista.dodaj_auto("Kia", "Ceed", 2001)
+
+        sorted_list = lista.sortuj_nazwa(True)
+        self.assertEqual(sorted_list, [
+            Auto("Ford", "Fusion", 2005),
+            Auto("Ford", "Fiesta", 2005),
+            Auto("BMW", "E30", 2005),
+            Auto("Fiat", "Croma", 2020),
+            Auto("Kia", "Ceed", 2001)
+        ])
 
     def test_sortuj_marka(self):
-        pass
+        lista = UlubioneAuta()
+
+        lista.dodaj_auto("Fiat", "Croma", 2020)
+        lista.dodaj_auto("Ford", "Fusion", 2005)
+        lista.dodaj_auto("Ford", "Fiesta", 2005)
+        lista.dodaj_auto("BMW", "E30", 2005)
+        lista.dodaj_auto("Kia", "Ceed", 2001)
+
+        sorted_list = lista.sortuj_marka()
+        self.assertEqual(sorted_list, [
+            Auto("BMW", "E30", 2005),
+            Auto("Fiat", "Croma", 2020),
+            Auto("Ford", "Fusion", 2005),
+            Auto("Ford", "Fiesta", 2005),
+            Auto("Kia", "Ceed", 2001)
+        ])
 
     def test_sortuj_marka_odwrotnie(self):
-        pass
+        lista = UlubioneAuta()
+
+        lista.dodaj_auto("Fiat", "Croma", 2020)
+        lista.dodaj_auto("Ford", "Fusion", 2005)
+        lista.dodaj_auto("Ford", "Fiesta", 2005)
+        lista.dodaj_auto("BMW", "E30", 2005)
+        lista.dodaj_auto("Kia", "Ceed", 2001)
+
+        sorted_list = lista.sortuj_marka(True)
+        self.assertEqual(sorted_list, [
+            Auto("Kia", "Ceed", 2001),
+            Auto("Ford", "Fusion", 2005),
+            Auto("Ford", "Fiesta", 2005),
+            Auto("Fiat", "Croma", 2020),
+            Auto("BMW", "E30", 2005),
+        ])
 
     def test_sortuj_rok(self):
-        pass
+        lista = UlubioneAuta()
+
+        lista.dodaj_auto("Fiat", "Croma", 2020)
+        lista.dodaj_auto("Ford", "Fusion", 2005)
+        lista.dodaj_auto("Ford", "Fiesta", 2004)
+        lista.dodaj_auto("BMW", "E30", 2010)
+        lista.dodaj_auto("Kia", "Ceed", 2001)
+
+        sorted_list = lista.sortuj_rok()
+        self.assertEqual(sorted_list, [
+            Auto("Kia", "Ceed", 2001),
+            Auto("Ford", "Fiesta", 2004),
+            Auto("Ford", "Fusion", 2005),
+            Auto("BMW", "E30", 2010),
+            Auto("Fiat", "Croma", 2020)
+        ])
 
     def test_sortuj_rok_odwrotnie(self):
-        pass
+        lista = UlubioneAuta()
+
+        lista.dodaj_auto("Fiat", "Croma", 2020)
+        lista.dodaj_auto("Ford", "Fusion", 2005)
+        lista.dodaj_auto("Ford", "Fiesta", 2004)
+        lista.dodaj_auto("BMW", "E30", 2010)
+        lista.dodaj_auto("Kia", "Ceed", 2001)
+
+        sorted_list = lista.sortuj_rok(True)
+        self.assertEqual(sorted_list, [
+            Auto("Fiat", "Croma", 2020),
+            Auto("BMW", "E30", 2010),
+            Auto("Ford", "Fusion", 2005),
+            Auto("Ford", "Fiesta", 2004),
+            Auto("Kia", "Ceed", 2001)
+        ])
